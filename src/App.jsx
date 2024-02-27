2cb9cd9d247ed6ad2db9827aed9e16ef81c3a412
Aiman SIngh <145761115+Aimans30@users.noreply.github.com>
2024-02-27 11:57:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import Editor from './Components/Editor'; // Importing the Editor component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pro-Note</h1>
+      <Editor /> {/* Using the Editor component */}
+    </div>
+  );
+}
+
+export default App;
